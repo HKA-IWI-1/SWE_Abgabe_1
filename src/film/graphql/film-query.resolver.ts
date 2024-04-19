ae5f1a60a704693b0c018cdb21097fd8caf4f31d
RonFrie <agnet.ronny@icloud.com>
2024-04-19 12:41:27 +0200

    Added Query.Resolver

---
+++ src/film/graphql/film-query.resolver.ts
@@ -0,0 +1,70 @@
+import { Args, Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
+import { UseFilters, UseInterceptors } from '@nestjs/common';
+import { Film } from '../entity/film.entity.js';
+import { FilmReadService } from '../service/film-read.service.js';
+import { HttpExceptionFilter } from './http-exception.filter.js';
+import { Public } from 'nest-keycloak-connect';
+import { ResponseTimeInterceptor } from '../../logger/response-time.interceptor.js';
+import { type Suchkriterien } from '../service/suchkriterien.js';
+import { getLogger } from '../../logger/logger.js';
+
+export interface IdInput {
+    readonly id: number;
+}
+
+export interface SuchkriterienInput {
+    readonly suchkriterien: Suchkriterien;
+}
+
+@Resolver((_: any) => Film)
+@UseFilters(HttpExceptionFilter)
+@UseInterceptors(ResponseTimeInterceptor)
+export class FilmQueryResolver {
+    readonly #service: FilmReadService;
+    readonly #logger = getLogger(FilmQueryResolver.name);
+
+    constructor(service: FilmReadService) {
+        this.#service = service;
+    }
+
+    @Query('film')
+    @Public()
+    async findById(@Args() { id }: IdInput) {
+        this.#logger.debug('findById: id=%d', id);
+
+        const film = await this.#service.findById({ id });
+
+        if (this.#logger.isLevelEnabled('debug')) {
+            this.#logger.debug(
+                'findById: film=%s, produktionsstudio=o',
+                film.toString(),
+                film.produktionsstudio,
+            );
+        }
+        return film;
+    }
+
+    @Query('filme')
+    @Public()
+    async find(@Args() input: SuchkriterienInput | undefined) {
+        this.#logger.debug('find: input=%o', input);
+        const filme = await this.#service.find(input?.suchkriterien);
+        this.#logger.debug('find: buecher=%o', filme);
+        return filme;
+    }
+
+    @ResolveField('rabatt')
+    rabatt(@Parent() film: Film, short: boolean | undefined) {
+        if (this.#logger.isLevelEnabled('debug')) {
+            this.#logger.debug(
+                'rabatt: film=%s, short=%s',
+                film.toString(),
+                short,
+            );
+        }
+        const rabatt = film.rabatt ?? 0;
+        const shortStr = short === undefined || short ? '%' : 'Prozent';
+        // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+        return `${(rabatt * 100).toFixed(2)} ${shortStr}`;
+    }
+}
