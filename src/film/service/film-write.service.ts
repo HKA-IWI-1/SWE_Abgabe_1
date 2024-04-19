ad78cc15e909255dca13ef3b0d97cc26f3a6088a
flomanuel <flomanuel.github@gmail.com>
2024-04-19 10:38:51 +0200

    added film-write.service

---
+++ src/film/service/film-write.service.ts
@@ -0,0 +1,205 @@
+/*
+ * Copyright (c) 2024 - present Florian Sauer
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the “Software”), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions
+ * of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+ * LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+/**
+ * Das Modul besteht aus der Klasse {@linkcode FilmWriteService} für die
+ * Schreiboperationen im Anwendungskern.
+ * @packageDocumentation
+ */
+
+import { DeleteResult, Repository } from 'typeorm';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+    VersionInvalidException,
+    VersionOutdatedException,
+} from './exceptions';
+import { Film } from '../entity/film.entity.js';
+import { FilmReadService } from './film-read.service';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Produktionsstudio } from '../entity/produktionsstudio.entity';
+import { Produzent } from '../entity/produzent.entity';
+import { getLogger } from '../../logger/logger';
+
+/** Typdefinitionen zum Aktualisieren eines Filmes mit `update`. */
+export interface UpdateParams {
+    /** ID des zu aktualisierenden Filmes. */
+    readonly id: number | undefined;
+    /** Film-Objekt mit den aktualisierten Werten. */
+    readonly film: Film;
+    /** Versionsnummer für die aktualisierenden Werte. */
+    readonly version: string;
+}
+
+/**
+ * Die Klasse `FilmWriteService` implementiert den Anwendungskern für das
+ * Schreiben von Filmen und greift mit _TypeORM_ auf die DB zu.
+ */
+@Injectable()
+export class FilmWriteService {
+    private static readonly VERSION_PATTERN = /^"\d{1,3}"/u;
+
+    readonly #repo: Repository<Film>;
+
+    readonly #readService: FilmReadService;
+
+    readonly #mailService: MailService;
+
+    readonly #logger = getLogger(FilmWriteService.name);
+
+    constructor(
+        @InjectRepository(Film) repo: Repository<Film>,
+        readService: FilmReadService,
+        mailService: MailService,
+    ) {
+        this.#repo = repo;
+        this.#readService = readService;
+        this.#mailService = mailService;
+    }
+
+    /**
+     * Ein neuer Film soll angelegt werden.
+     * @param film Der neu abzulegende Film.
+     * @returns Die ID des neu angelegten Filmes.
+     */
+    async create(film: Film): Promise<number> {
+        this.#logger.debug('create: film=%o', film);
+
+        const filmDB = await this.#repo.save(film); // implizite Transaktion
+        this.#logger.debug('create: filmDB=%o', filmDB);
+
+        await this.#sendmail(filmDB);
+
+        return filmDB.id!;
+    }
+
+    /**
+     * Ein vorhandener Film soll aktualisiert werden. "Destructured" Argument
+     * mit id (ID des zu aktualisierenden Filmes), film (zu aktualisierender Film)
+     * und version (Versionsnummer für optimistische Synchronisation).
+     * @returns Die neue Versionsnummer gemäß optimistischer Synchronisation.
+     * @throws NotFoundException falls kein Film zur ID vorhanden ist.
+     * @throws VersionInvalidException falls die Versionsnummer ungültig ist.
+     * @throws VersionOutdatedException falls die Versionsnummer veraltet ist.
+     */
+    async update({ id, film, version }: UpdateParams): Promise<number> {
+        this.#logger.debug(
+            'update: id=%d, film=%o, version=%s',
+            id,
+            film,
+            version,
+        );
+        if (id === undefined) {
+            this.#logger.debug('update: Keine gueltige ID');
+            throw new NotFoundException(
+                `Es gibt keinen Film mit der ID ${id}.`,
+            );
+        }
+
+        const validateResult = await this.#validateUpdate(film, id, version);
+        this.#logger.debug('update: validateResult=%o', validateResult);
+        if (!(validateResult instanceof Film)) {
+            return validateResult;
+        }
+
+        const filmNeu = validateResult;
+        const merged = this.#repo.merge(filmNeu, film);
+        this.#logger.debug('update: merged=%o', merged);
+        const updated = await this.#repo.save(merged); // implizite Transaktion
+        this.#logger.debug('update: updated=%o', updated);
+
+        return updated.version!;
+    }
+
+    /**
+     * Ein Film wird asynchron anhand seiner ID gelöscht.
+     *
+     * @param id ID des zu löschenden Filmes.
+     * @returns true, falls der Film vorhanden war und gelöscht wurde. Sonst false.
+     */
+    async delete(id: number) {
+        this.#logger.debug('delete: id=%d', id);
+        const film = await this.#readService.findById({
+            id,
+            mitProduzenten: true,
+        });
+
+        let deleteResult: DeleteResult | undefined;
+        await this.#repo.manager.transaction(async (transactionalMgr) => {
+            const produktionsstudioId = film.produktionsstudio?.id;
+            if (produktionsstudioId !== undefined) {
+                await transactionalMgr.delete(
+                    Produktionsstudio,
+                    produktionsstudioId,
+                );
+            }
+            const produzenten = film.produzenten ?? [];
+            for (const produzent of produzenten) {
+                await transactionalMgr.delete(Produzent, produzent.id);
+            }
+
+            deleteResult = await transactionalMgr.delete(Film, id);
+            this.#logger.debug('delete: deleteResult=%o', deleteResult);
+        });
+
+        return (
+            deleteResult?.affected !== undefined &&
+            deleteResult.affected !== null &&
+            deleteResult.affected > 0
+        );
+    }
+
+    async #sendmail(film: Film) {
+        const subject = `Neuer Film ${film.id}`;
+        const body = `Der Film mit dem Titel <strong>${film.titel}</strong> ist angelegt.`;
+        await this.#mailService.sendmail({ subject, body });
+    }
+
+    async #validateUpdate(
+        film: Film,
+        id: number,
+        versionStr: string,
+    ): Promise<Film> {
+        this.#logger.debug(
+            '#validateUpdate: film=%o, id=%s, versionStr=%s',
+            film,
+            id,
+            versionStr,
+        );
+        if (!FilmWriteService.VERSION_PATTERN.test(versionStr)) {
+            throw new VersionInvalidException(versionStr);
+        }
+
+        const version = Number.parseInt(versionStr.slice(1, -1), 10);
+        this.#logger.debug(
+            '#validateUpdate: film=%o, version=%d',
+            film,
+            version,
+        );
+
+        const filmDb = await this.#readService.findById({ id });
+
+        const versionDb = filmDb.version!;
+        if (version < versionDb) {
+            this.#logger.debug('#validateUpdate: versionDb=%d', version);
+            throw new VersionOutdatedException(version);
+        }
+        this.#logger.debug('#validateUpdate: filmDb=%o', filmDb);
+        return filmDb;
+    }
+}
